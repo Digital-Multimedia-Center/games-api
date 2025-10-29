a82e6cf7254c8cb06c61b8abdbf0d42e3ebf9f80
Swefton <amrit.madhav.srivastava@gmail.com>
2025-10-29 17:42:57 -0400

    added missing platform, wii

--- postman.py
+++ postman.py
@@ -35,8 +35,8 @@ limit 20;
 
 query = """
 fields name, category, platforms, status, game_type, rating;
-search "Super Mario 64.";
-where platforms = (4) & game_type != (5, 12) & (status != (2,3,6) | status = null);
+search "Mario strikers charged.";
+where platforms = (5) & game_type != (5, 12) & (status != (2,3,6) | status = null);
 limit 100;
 """
 
