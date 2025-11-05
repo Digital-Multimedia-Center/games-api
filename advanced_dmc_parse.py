ebaec32111ecb5ad7eced7d68bf724a5ff985cab
Swefton <amrit.madhav.srivastava@gmail.com>
2025-11-05 12:01:54 -0500

    converting advanced api to db schema

--- advanced_dmc_parse.py
+++ advanced_dmc_parse.py
@@ -4,53 +4,64 @@ import xml.etree.ElementTree as ET
 
 
 IDENTIFIER = "folio.in00006997485"
-
 url = f"https://catalog.lib.msu.edu/OAI/Server?verb=GetRecord&identifier={IDENTIFIER}&metadataPrefix=marc21"
 
-print(url)
 
-response = requests.get(url)
-xml_data = response.text
 
-# Parse XML
-root = ET.fromstring(xml_data)
+def metadata_from_msu(url):
 
-# Namespaces
-ns = {
-    'oai': 'http://www.openarchives.org/OAI/2.0/',
-    'marc': 'http://www.loc.gov/MARC21/slim'
-}
+    response = requests.get(url)
+    xml_data = response.text
 
-# Find the MARC record element
-record_elem = root.find('.//oai:GetRecord/oai:record/marc:record', ns)
+    # Parse XML
+    root = ET.fromstring(xml_data)
 
-# Alternative: sometimes the MARC record is nested with default namespace
-if record_elem is None:
-    # Look for any element with the MARC namespace
-    record_elem = root.find('.//{http://www.loc.gov/MARC21/slim}record')
+    # Namespaces
+    ns = {
+        'oai': 'http://www.openarchives.org/OAI/2.0/',
+        'marc': 'http://www.loc.gov/MARC21/slim'
+    }
 
-if record_elem is None:
-    raise ValueError("MARC record not found.")
+    # Find the MARC record element
+    record_elem = root.find('.//oai:GetRecord/oai:record/marc:record', ns)
 
-# Leader
-leader_elem = record_elem.find('{http://www.loc.gov/MARC21/slim}leader')
-assert leader_elem is not None, "Leader not found!"
-leader = leader_elem.text
+    # Alternative: sometimes the MARC record is nested with default namespace
+    if record_elem is None:
+        # Look for any element with the MARC namespace
+        record_elem = root.find('.//{http://www.loc.gov/MARC21/slim}record')
 
-# Controlfields
-# print("\nControlfields:")
-# for cf in record_elem.findall('{http://www.loc.gov/MARC21/slim}controlfield'):
-#     print(f"Tag {cf.get('tag')}: {cf.text}")
+    if record_elem is None:
+        raise ValueError("MARC record not found.")
 
-# Datafields
-print("\nDatafields:")
+    # Leader
+    leader_elem = record_elem.find('{http://www.loc.gov/MARC21/slim}leader')
+    assert leader_elem is not None, "Leader not found!"
+    leader = leader_elem.text
 
-datafields_by_tag = defaultdict(list)
+    # Controlfields
+    # print("Controlfields:")
+    # for cf in record_elem.findall('{http://www.loc.gov/MARC21/slim}controlfield'):
+    #     print(f"Tag {cf.get('tag')}: {cf.text}")
 
-for df in record_elem.findall('{http://www.loc.gov/MARC21/slim}datafield'):
-    tag = df.get('tag')
-    subfields = {sf.get('code'): sf.text for sf in df.findall('{http://www.loc.gov/MARC21/slim}subfield')}
-    datafields_by_tag[tag].append(subfields)
+    # Datafields
+    datafields_by_tag = defaultdict(list)
 
-print(datafields_by_tag.get("246"))
+    for df in record_elem.findall('{http://www.loc.gov/MARC21/slim}datafield'):
+        tag = df.get('tag')
+        subfields = {sf.get('code'): sf.text for sf in df.findall('{http://www.loc.gov/MARC21/slim}subfield')}
+        datafields_by_tag[tag].append(subfields)
 
+
+    
+    title = [item['a'] for item in datafields_by_tag.get("245")] if datafields_by_tag.get("245") else []
+    alternative_titles = [item['a'] for item in datafields_by_tag.get("246")]
+    authors = [item['a'] for item in datafields_by_tag.get("710")]
+    edition = [item['a'] for item in datafields_by_tag.get("250")]
+    platform = [item['a'] for item in datafields_by_tag.get("753")]
+    print(title)
+    print(alternative_titles)
+    print(authors)
+    print(edition)
+    print(platform)
+
+metadata_from_msu(url)
