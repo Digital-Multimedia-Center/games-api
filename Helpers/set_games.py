33b5a6ec221732660e45710a244908e3d6052f87
Swefton <amrit.madhav.srivastava@gmail.com>
2026-04-10 12:43:21 -0400

    removed extraneous helpers

--- Helpers/set_games.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("games.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-editions = set(item["dmc"]["edition"] for item in data)
-
-for i in editions:
-    print(i)
